e15dc2d950931d6fde1ce46fa0bdfed1584579a7
Sergei Prokopchuk <seraapa@mail.ru>
2024-03-19 16:37:29 +0300

    added homework

---
+++ lection_1/homework.js
@@ -0,0 +1,56 @@
+
+// Неявное преобразование типов
+// console.log(NaN + 1 + null + 1);
+// console.log(NaN + 1 + null + 1 + "");
+// console.log("" + NaN + 1 + null + 1 + "");
+// console.log("number" + 15 + 3)
+// console.log(true + false)
+// console.log([] + null + 1)
+// console.log("foo" + + "bar")
+// console.log(!!"false" == !!"true")
+// console.log(null == '')
+// console.log([] == ![])
+
+// Логические операторы
+// console.log("a" && "b" && "c")
+// console.log("a" || "b" || "c")
+// console.log("a" && "" && "c")
+// console.log("a" && "" || "c")
+// console.log("" ?? "a")
+// console.log(null ?? "a")
+
+// Всплытие
+
+// var a = 1;
+//
+// function foo() {
+//     console.log(a);
+// }
+//
+// function bar() {
+//     var a = 2;
+//     foo();
+// }
+//
+// bar(); // ?
+////////////////////
+
+// function foo(a) {
+//     if (a > 0) {
+//         let a = a + 10
+//         return a
+//     }
+//     return a
+// }
+// console.log(foo(15)) //?
+////////////////////
+
+// function giveMeX(showX) {
+//     if(showX) {
+//         let x = 5;
+//     }
+//     return x;
+// }
+//
+// console.log(giveMeX(false)); // ?
+// console.log(giveMeX(true)); // ?
